9c26582586171d76da74c25369b13cb487602a9a
ZachTRice <contact@zachtrice.com>
2017-06-26 12:46:22 -0400

    Moved url replacement click event into worldview-components

--- src/scripts/components/link/wv.link.share.js
+++ src/scripts/components/link/wv.link.share.js
@@ -39,6 +39,37 @@ export default class Links extends React.Component {
     return 'mailto:' + util.objectToGetParams({ subject, body });
   }
 
+  // When an icon-link is clicked, replace the URL with current encoded link.
+  replaceUrl() {
+    var model = linkmodel();
+    var promise = model.shorten();
+    var getLink = encodeURIComponent(model.get());
+    var shareMessage = 'Check out what I found in NASA Worldview!';
+    var twMessage = 'Check out what I found in #NASAWorldview -';
+    var emailBody = shareMessage + "%20-%20" + getLink;
+    // var fbUrl = this.facebookUrlParams('121285908450463', getLink, getLink, 'popup');
+    // var twUrl = this.twitterUrlParams(getLink, twMessage);
+    // var rdUrl = this.redditUrlParams(getLink, shareMessage);
+    // var emailUrl = this.emailUrlParams(shareMessage, emailBody);
+
+    document.getElementById("fb-share").setAttribute("href", "https://www.facebook.com/dialog/share?" + "app_id=" + '121285908450463' + "&href=" + getLink + "&redirect_uri=" + getLink + "&display=popup");
+    document.getElementById("tw-share").setAttribute("href", "https://twitter.com/intent/tweet?" + "url=" + getLink + "&text=" + twMessage);
+    document.getElementById("rd-share").setAttribute("href", "https://www.reddit.com/r/nasa/submit?" + "url=" + getLink + "&title=" + shareMessage);
+    document.getElementById("email-share").setAttribute("href", "mailto:?" + "subject=" + shareMessage + "&body=" + emailBody);
+
+    // If a short link can be generated, replace the full link.
+    promise.done(function(result) {
+      if (result.status_code === 200) {
+        getLink = encodeURIComponent(result.data.url);
+        emailBody = shareMessage + "%20-%20" + getLink;
+
+        document.getElementById("tw-share").setAttribute("href", "https://twitter.com/intent/tweet?" + "url=" + getLink + "&text=" + twMessage);
+        document.getElementById("email-share").setAttribute("href", "mailto:?" + "subject=" + shareMessage + "&body=" + emailBody);
+        return false;
+      }
+    });
+  }
+
   socialButton(id, className, href, target, title) {
     var link;
 
@@ -66,10 +97,10 @@ export default class Links extends React.Component {
 
     return (
       <div id="social-share">
-        <a id="fb-share" className="icon-link fa fa-facebook fa-2x" href={fbUrl} target="_blank" title="Share via Facebook!" />
-        <a id="tw-share" className="icon-link fa fa-twitter fa-2x" href={twUrl} target="_blank" title="Share via Twitter!" />
-        <a id="rd-share" className="icon-link fa fa-reddit fa-2x" href={rdUrl} target="_blank" title="Share via Reddit!" />
-        <a id="email-share" className="icon-link fa fa-envelope fa-2x" href={emailUrl} target="_self" title="Share via Email!" />
+        <a id="fb-share" className="icon-link fa fa-facebook fa-2x" href={fbUrl} onClick={this.replaceUrl} target="_blank" title="Share via Facebook!" />
+        <a id="tw-share" className="icon-link fa fa-twitter fa-2x" href={twUrl} onClick={this.replaceUrl} target="_blank" title="Share via Twitter!" />
+        <a id="rd-share" className="icon-link fa fa-reddit fa-2x" href={rdUrl} onClick={this.replaceUrl} target="_blank" title="Share via Reddit!" />
+        <a id="email-share" className="icon-link fa fa-envelope fa-2x" href={emailUrl} onClick={this.replaceUrl} target="_self" title="Share via Email!" />
       </div>
     );
   }
