99f386a1fcecd9319732c53ea06434be3515ea68
Benjamin <benjamin.a.king@nasa.gov>
2017-07-20 17:40:09 -0400

    passed type through onClick nasa-gibs/worldview#139

--- src/scripts/components/share/wv.share.links.js
+++ src/scripts/components/share/wv.share.links.js
@@ -15,14 +15,17 @@
 import React from 'react';
 
 export default class ShareLinks extends React.Component {
+  onClick(type) {
+    this.props.onClick(type);
+  }
 
   render() {
     return (
       <div id="social-share">
-        <a id="fb-share" className="icon-link fa fa-facebook fa-2x" href={this.props.fbLink} onClick={this.props.onClick} target="_blank" title="Share via Facebook!" />
-        <a id="tw-share" className="icon-link fa fa-twitter fa-2x" href={this.props.twLink} onClick={this.props.onClick} target="_blank" title="Share via Twitter!" />
-        <a id="rd-share" className="icon-link fa fa-reddit fa-2x" href={this.props.rdLink} onClick={this.props.onClick} target="_blank" title="Share via Reddit!" />
-        <a id="email-share" className="icon-link fa fa-envelope fa-2x" href={this.props.emailLink} onClick={this.props.onClick} target="_self" title="Share via Email!" />
+        <a id="fb-share" className="icon-link fa fa-facebook fa-2x" href={this.props.fbLink} onClick={() => this.onClick('facebook')}  title="Share via Facebook!" />
+        <a id="tw-share" className="icon-link fa fa-twitter fa-2x" href={this.props.twLink} onClick={() => this.onClick('twitter')}  title="Share via Twitter!" />
+        <a id="rd-share" className="icon-link fa fa-reddit fa-2x" href={this.props.rdLink} onClick={() => this.onClick('reddit')}  title="Share via Reddit!" />
+        <a id="email-share" className="icon-link fa fa-envelope fa-2x" href={this.props.emailLink} onClick={() => this.onClick('email')} title="Share via Email!" />
       </div>
     );
   }
