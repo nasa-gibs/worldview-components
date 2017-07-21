5541ed8bad2363aad5c822b5e5234ccef012ada2
Benjamin <benjamin.a.king@nasa.gov>
2017-07-21 10:07:39 -0400

    prevented defaults of Share Links onClick nasa-gibs/worldview#139

--- src/scripts/components/share/wv.share.links.js
+++ src/scripts/components/share/wv.share.links.js
@@ -15,17 +15,18 @@
 import React from 'react';
 
 export default class ShareLinks extends React.Component {
-  onClick(type) {
+  onClick(event, type) {
+    event.preventDefault();
     this.props.onClick(type);
   }
 
   render() {
     return (
       <div id="social-share">
-        <a id="fb-share" className="icon-link fa fa-facebook fa-2x" href={this.props.fbLink} onClick={() => this.onClick('facebook')}  title="Share via Facebook!" />
-        <a id="tw-share" className="icon-link fa fa-twitter fa-2x" href={this.props.twLink} onClick={() => this.onClick('twitter')}  title="Share via Twitter!" />
-        <a id="rd-share" className="icon-link fa fa-reddit fa-2x" href={this.props.rdLink} onClick={() => this.onClick('reddit')}  title="Share via Reddit!" />
-        <a id="email-share" className="icon-link fa fa-envelope fa-2x" href={this.props.emailLink} onClick={() => this.onClick('email')} title="Share via Email!" />
+        <a id="fb-share" className="icon-link fa fa-facebook fa-2x" href={this.props.fbLink} onClick={(e) => this.onClick(e, 'facebook')}  title="Share via Facebook!" />
+        <a id="tw-share" className="icon-link fa fa-twitter fa-2x" href={this.props.twLink} onClick={(e) => this.onClick(e, 'twitter')}  title="Share via Twitter!" />
+        <a id="rd-share" className="icon-link fa fa-reddit fa-2x" href={this.props.rdLink} onClick={(e) => this.onClick(e, 'reddit')}  title="Share via Reddit!" />
+        <a id="email-share" className="icon-link fa fa-envelope fa-2x" href={this.props.emailLink} onClick={(e) => this.onClick(e, 'email')} title="Share via Email!" />
       </div>
     );
   }
