085bb5e8d039a5b524f662021397249814436e45
Benjamin King <benjamin.a.king@nasa.gov>
2018-01-30 14:40:51 -0500

    Add image download and GIF download components
    * Include selector, checkbox, and button sub-widgets

---
+++ src/scripts/components/main/button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+/*
+ * A react component, Builds a rather specific
+ * interactive widget
+ *
+ * @class AnimationWidget
+ * @extends React.Component
+ */
+export default class Button extends React.Component {
+  render() {
+    return (
+      <button
+        onClick={this.props.onClick}
+        id={this.props.id}
+        disabled={(!this.props.valid)}
+        className= {(this.props.valid) ? 'wv-button ' + this.props.className : 'wv-disabled wv-button' + this.props.className}
+      >
+        <span className='button-text'>{this.props.text}</span>
+      </button>
+    );
+  }
+}
+
+Button.defaultProps = {
+  className: 'gray'
+};
