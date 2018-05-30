ab087d700dc817e3a210aa3ce341cf7167666229
ZachTRice <zachtrice@gmail.com>
2018-05-30 16:38:44 -0400

    Format date ranges and add toggle functionality

--- src/scripts/components/layer/row.js
+++ src/scripts/components/layer/row.js
@@ -15,7 +15,8 @@ class LayerRow extends React.Component {
     super(props);
     this.state = {
       checked: props.isEnabled,
-      isExpanded: props.isExpanded
+      isMetadataExpanded: props.isMetadataExpanded,
+      isDateRangesExpanded: props.isDateRangesExpanded
     };
   }
 
@@ -40,9 +41,22 @@ class LayerRow extends React.Component {
    */
   toggleMetadataButtons (e) {
     e.stopPropagation(); // Prevent layer from being activated
-    var { layer, toggleExpansion } = this.props;
-    this.setState({isExpanded: !this.state.isExpanded});
-    toggleExpansion(layer.id);
+    var { layer, toggleMetadataExpansion } = this.props;
+    this.setState({isMetadataExpanded: !this.state.isMetadataExpanded});
+    toggleMetadataExpansion(layer.id);
+  }
+
+  /**
+   * Toggle switch for the metadata info button and close arrow
+   * @method toggleMetadataButtons
+   * @param {e} event
+   * @return {void}
+   */
+  toggleDateRanges (e) {
+    e.stopPropagation(); // Prevent layer from being activated
+    var { layer, toggleDateRangesExpansion } = this.props;
+    this.setState({isDateRangesExpanded: !this.state.isDateRangesExpanded});
+    toggleDateRangesExpansion(layer.id);
   }
 
   /**
@@ -82,9 +96,9 @@ class LayerRow extends React.Component {
         endDate = 'Present';
       }
       if (layer.id) endDateId = layer.id + '-endDate';
-      dateRange = '<p>Temporal coverage: <span class="layer-date-start" id=' +
+      dateRange = 'Temporal coverage: <span class="layer-date-start" id=' +
       startDateId + '>' + startDate + '</span> - <span class="layer-end-date" id=' +
-      endDateId + '>' + endDate + '</span></p>';
+      endDateId + '>' + endDate + '</span>';
     }
 
     return dateRange;
@@ -93,22 +107,40 @@ class LayerRow extends React.Component {
   componentWillReceiveProps(nextProps) {
     this.state = {
       checked: nextProps.isEnabled,
-      isExpanded: nextProps.isExpanded
+      isMetadataExpanded: nextProps.isMetadataExpanded,
+      isDateRangesExpanded: nextProps.isDateRangesExpanded
     };
   }
 
   render() {
-    var { checked, isExpanded } = this.state;
+    var { checked, isMetadataExpanded, isDateRangesExpanded } = this.state;
     var { layer } = this.props;
     var { title, description, subtitle, metadata } = layer;
     var listItems;
     var headerClass = 'layers-all-header has-checkbox';
-    if (layer.dateRanges) {
-      listItems = layer.dateRanges.map((l) =>
-        <ListGroupItem key={l.startDate + ' - ' + l.endDate}>
-          {l.startDate + '-' + l.endDate}
-        </ListGroupItem>);
-    }
+    if (layer.period !== 'subdaily') {
+      if (layer.dateRanges) {
+        listItems = layer.dateRanges.map((l) =>
+          <ListGroupItem key={l.startDate + ' - ' + l.endDate}>
+            {(util.parseDate(l.startDate)).getDate() + ' ' + util.giveMonth(util.parseDate(l.startDate)) + ' ' +
+            (util.parseDate(l.startDate)).getFullYear() + ' - ' + (util.parseDate(l.endDate)).getDate() + ' ' +
+            util.giveMonth(util.parseDate(l.endDate)) + ' ' + (util.parseDate(l.endDate)).getFullYear()}
+          </ListGroupItem>);
+      }
+    } else {
+      if (layer.dateRanges) {
+        listItems = layer.dateRanges.map((l) =>
+          <ListGroupItem key={l.startDate + ' - ' + l.endDate}>
+            {(util.parseDate(l.startDate)).getDate() + ' ' + util.giveMonth(util.parseDate(l.startDate)) + ' ' +
+            (util.parseDate(l.startDate)).getFullYear() + ' ' + util.pad((util.parseDate(l.startDate)).getHours(), 2, '0') + ':' +
+            util.pad((util.parseDate(l.startDate)).getMinutes(), 2, '0') + ' - ' + (util.parseDate(l.endDate)).getDate() + ' ' +
+            util.giveMonth(util.parseDate(l.endDate)) + ' ' + (util.parseDate(l.endDate)).getDate() + ' ' + util.giveMonth(util.parseDate(l.endDate)) + ' ' +
+            (util.parseDate(l.endDate)).getFullYear() + ' ' + util.pad((util.parseDate(l.endDate)).getHours(), 2, '0') + ':' +
+            util.pad((util.parseDate(l.endDate)).getMinutes(), 2, '0')}
+          </ListGroupItem>);
+      }
+    };
+
     if (checked) headerClass += ' checked';
     return (
       <div className='layers-all-layer'>
@@ -121,15 +153,24 @@ class LayerRow extends React.Component {
           </h3>
           {subtitle && <h5>{subtitle}</h5>}
         </div>
-        {isExpanded && metadata &&
+        {isMetadataExpanded && metadata &&
           <div className="source-metadata visible">
-            <div>{this.dateRangeText(layer)}</div>
+            {layer.startDate &&
+            <p className="layer-date-range">
+              <span dangerouslySetInnerHTML={{__html: this.dateRangeText(layer)}} />
+              {layer.dateRanges &&
+                <a id="layer-date-ranges-button" className="layer-date-ranges-button" onClick={e => this.toggleDateRanges(e)}> *</a>
+              }
+            </p>
+            }
+            {isDateRangesExpanded &&
             <div className='layer-date-wrap'>
-              <a id='layer-date-ranges-button' className='layer-date-ranges-button'>*Show All Date Ranges</a>
+              <p>Date Ranges:</p>
               <ListGroup className='layer-date-ranges'>
                 {listItems}
               </ListGroup>
             </div>
+            }
             <div dangerouslySetInnerHTML={{__html: metadata}} />
             <div className="metadata-more" onClick={e => this.toggleMetadataButtons(e)}>
               <span className="ellipsis up">^</span>
@@ -144,10 +185,12 @@ class LayerRow extends React.Component {
 LayerRow.propTypes = {
   layer: PropTypes.object,
   isEnabled: PropTypes.bool,
-  isExpanded: PropTypes.bool,
+  isMetadataExpanded: PropTypes.bool,
+  isDateRangesExpanded: PropTypes.bool,
   onState: PropTypes.func,
   offState: PropTypes.func,
-  toggleExpansion: PropTypes.func
+  toggleMetadataExpansion: PropTypes.func,
+  toggleDateRangesExpansion: PropTypes.func
 };
 
 export default LayerRow;
