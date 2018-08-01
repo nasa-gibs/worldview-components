03b2e16343fc7044b39fbfab0231bfadd79b1c03
Benjamin King <benking4@gmail.com>
2018-08-01 12:13:45 -0400

    add sidebar and opacity-sidebar to index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,28 @@
-export {default as AnimationWidget} from './components/animation-widget/animation-widget.js';
-export {default as RangeSelector} from './components/range-selection/range-selection.js';
-export {default as DateSelector} from './components/date-selector/date-selector.js';
-export {default as ToolTip} from './components/tooltip/tooltip.js';
-export {default as Share} from './components/share/share.js';
-export {default as Util} from './components/util/util.js';
-export {default as GA} from './components/util/google-analytics.js';
-export {default as DateLine} from './components/dateline/dateline.js';
-export {default as LineText} from './components/dateline/text.js';
-export {default as LayerList} from './components/layer/list.js';
-export {default as LayerRow} from './components/layer/row.js';
-export {default as ImageResSelection} from './components/image-panel/select.js';
-export {default as GifResSelection} from './components/animation-widget/gif-panel.js';
+export {
+  default as AnimationWidget
+} from './components/animation-widget/animation-widget.js';
+export {
+  default as RangeSelector
+} from './components/range-selection/range-selection.js';
+export {
+  default as TimelineDragger
+} from './components/range-selection/dragger.js';
+export {
+  default as DateSelector
+} from './components/date-selector/date-selector.js';
+export { default as ToolTip } from './components/tooltip/tooltip.js';
+export { default as Share } from './components/share/share.js';
+export { default as Util } from './components/util/util.js';
+export { default as GA } from './components/util/google-analytics.js';
+export { default as DateLine } from './components/dateline/dateline.js';
+export { default as LineText } from './components/dateline/text.js';
+export { default as LayerList } from './components/layer/list.js';
+export { default as LayerRow } from './components/layer/row.js';
+export {
+  default as ImageResSelection
+} from './components/image-panel/select.js';
+export {
+  default as GifResSelection
+} from './components/animation-widget/gif-panel.js';
+export { default as Sidebar } from './components/sidebar/sidebar.js';
+export { default as OpacitySlider } from './components/compare/opacity-slider';
